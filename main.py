dc801353875bd3a4c973ae9ebe3d21ec623de2dc
Seif Hossam <112402478+seif8886@users.noreply.github.com>
2024-12-30 23:49:16 +0200

    Editing MyClient function

--- main.py
+++ main.py
@@ -3,29 +3,20 @@ from os import environ
 from dotenv import load_dotenv
 from imagereader import *
 from messages import *
-
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
+    async def on_message(self, message): # checking message content
         if not message.author.bot:
-            # checking message content
             if len(message.attachments) > 0:
                 image_statement = read_image_text(message.attachments[0].url)
                 if not image_statement == "Nothing":
                     messages_agenda_dict = get_messages_agenda_dict()
                     await message.reply(messages_agenda_dict[image_statement]["reply_message"])
-
-
+                    
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = MyClient(intents=intents)
-
 load_dotenv()
-
 token = environ["TOKEN"]
-
 client.run(token)
